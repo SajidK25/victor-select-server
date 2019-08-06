1c66d9b3448052600d90abbe67a0d7c3153ec628
bakerman59 <brian@bbaker.net>
2019-08-06 10:26:04 -0700

    final auth methods

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -8,12 +8,17 @@ const Query = {
     if (!req.userId) {
       return null
     }
-    return db.query.user({ where: { id: req.userId } })
+    return db.query.user(
+      {
+        where: { id: req.userId }
+      },
+      info
+    )
   },
   users: forwardTo('db'),
-  async userExists(parent, args, ctx, info) {
-    console.log('CTX -----', ctx.db.schema._typeMap.Role)
+  async userExists(_, args, ctx) {
     args.email = args.email.toLowerCase()
+    // if it's a visitor account we won't enforce the duplicate
     const user = await ctx.db.query.users({
       where: { email: args.email, role_not: 'VISITOR' }
     })
